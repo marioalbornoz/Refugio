aad590b5fdafefa234f543f8b01087a5646ec493
marioalbornoz <mario.albornozc@utem.cl>
2019-07-17 00:21:01 -0400

    Se agregan aplicaciones: mascota, adopciones

--- refugio/settings.py
+++ refugio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.mascota',
+    'apps.adopciones',
 ]
 
 MIDDLEWARE_CLASSES = [
