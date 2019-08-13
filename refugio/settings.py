6f07292177a2ab4f20127362d3fe5cd33af4cf37
marioalbornoz <mario.albornozc@utem.cl>
2019-08-12 23:37:18 -0400

    se agrega app usuario, login.

--- refugio/settings.py
+++ refugio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.mascota',
     'apps.adopciones',
+    'apps.usuario',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -126,3 +128,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_listar') #pagina a la cual nos redirijira una vez logueado
